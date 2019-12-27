53cb59d7324cc20ae93d2edb0bc09cb27b1c7e80
stepanchaparyan <stepanchaparyan@gmail.com>
2019-12-28 00:21:19 +0400

    Added auth router and added create-react-app

---
+++ routes/auth.routes.js
@@ -0,0 +1,81 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { check, validatorResult } = require("express-validator");
+const User = require("../models/User");
+const router = Router();
+
+// /api/auth/register
+router.post(
+  "/register",
+  [
+    check("email", "Email is not valid").isEmail(),
+    check("password", "Need minimum 6 symbols").isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    try {
+      const errors = validatorResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Not valid data for register"
+        });
+      }
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res.status(400).json({ message: "User already registered" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: "User created" });
+    } catch (e) {
+      res.status(500).json({ message: "Error from server 500, try again" });
+    }
+  }
+);
+
+// /api/auth/login
+router.post(
+  "/login",
+  [
+    check("email", "enter valid email")
+      .normalizeEmail()
+      .isEmail(),
+    check("password", "Enter password").exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validatorResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Not valid data for login"
+        });
+      }
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "User not exist" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Wrong password, try again" });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id }, 
+        config.get("jwtSecret"), 
+        { expiresIn: "1h" }
+      );
+      res.json({ token, userId: user.id });
+      
+    } catch (e) {
+      res.status(500).json({ message: "Error from server 500, try again" });
+    }
+  }
+);
+
+module.exports = router;
